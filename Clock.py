dc723b8ff7c9429ed946685d19093ab401913dda
SriSuriyaS <97033365+SriSuriyaS@users.noreply.github.com>
2022-01-18 17:58:45 +0530

    Update Clock.py

--- Clock.py
+++ Clock.py
@@ -1,22 +1,44 @@
-#Program to display present time
-#This is done by using tkinter module
-
+# importing whole module
 from tkinter import *
 from tkinter.ttk import *
+
+# importing strftime function to
+# retrieve system's time
 from time import strftime
 
+from datetime import date
+
+# creating tkinter window
 root = Tk()
 root.title('Clock')
 
-def time():
+# This function is used to
+# display time on the label
+def date_time():
 	#string = strftime('%H:%M:%S %p')
 	string = strftime('%I:%M:%S %p')
+	today = date.today()
 	lbl.config(text = string)
-	lbl.after(1000, time)
+	lbl1.config(text = today)
+	lbl.after(34, date_time)
 
-lbl = Label(root, font = ('lusida', 50, 'bold'),background = 'blue',foreground = 'white')
+# Styling the label widget so that clock
+# will look more attractive
+lbl = Label(root,font = ('lusida', 50, 'bold'),
+			background = 'white',
+			foreground = 'black')
+lbl1 = Label(root, font = ('lusida', 30, 'bold'),
+			background = 'white',
+			foreground = 'black')
 
+
+# Placing clock at the centre
+# of the tkinter window
 lbl.pack(anchor = 'center')
-time()
+lbl1.pack(anchor = 'center')
 
-mainloop()
+
+date_time()
+##
+
+root.mainloop()
